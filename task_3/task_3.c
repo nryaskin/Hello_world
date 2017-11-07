ac4c6a20eddfbefcc4f03cf3b55772d26c1b605e
nryaskin <nik.ryaskin@gmail.com>
2017-11-07 12:54:25 -0500

    Added input reading function

--- task_3/task_3.c
+++ task_3/task_3.c
@@ -1,5 +1,30 @@
 #include "defs.h"
+#define LENGTH 20
+#define S_LENGTH 100
+
+int read_file(char*** arr);
 
 void main(){
+    char** c_arr;
+    int count = read_file(& c_arr);
+    for(int i = 0; i < count; i++)
+        printf("%s\n", c_arr[i]);
+}
 
+int read_file(char*** arr){
+    char** result;	
+    int count = 0, j = 0;
+    size_t size = sizeof(char*);
+    result = malloc(size);
+    while(!feof(stdin)){
+        if(count > LENGTH * j)
+            result = realloc(result, LENGTH * ++j * size);
+	char * tmp = malloc(sizeof(char)*S_LENGTH);
+        scanf("%[^\n]s", tmp);
+        getc(stdin);
+        result[count++] = tmp;
+    }
+    result = realloc(result, --count * size);
+    *arr = result;
+    return count;
 }
